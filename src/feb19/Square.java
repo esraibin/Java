b1fc992d861c81dfb02bdfabc625712a60747c6b
Esra IBIN <iesra@na.edu>
2020-02-19 23:13:37 -0500

    loops

---
+++ src/feb19/Square.java
@@ -0,0 +1,14 @@
+package feb19;
+
+public class Square {//Print square with star using loop
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <10 ; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+    }
+}
